528eb5047d185681d69a801c66367be1052c042d
Luke Wagner <luke@mozilla.com>
2013-03-07 09:15:20 -0800

    Fix about:memory reporting to not include mmap'd memory

--- js/src/jsobjinlines.h
+++ js/src/jsobjinlines.h
@@ -1011,14 +1011,10 @@ JSObject::finish(js::FreeOp *fop)
         fop->free_(slots);
     if (hasDynamicElements()) {
         js::ObjectElements *elements = getElementsHeader();
-#ifdef JS_CPU_X64
         if (JS_UNLIKELY(elements->isAsmJSArrayBuffer()))
             js::ArrayBufferObject::releaseAsmJSArrayBuffer(this);
         else
             fop->free_(elements);
-#else
-        fop->free_(elements);
-#endif
     }
 }
 
@@ -1103,8 +1099,19 @@ JSObject::sizeOfExcludingThis(JSMallocSizeOfFun mallocSizeOf, JS::ObjectsExtraSi
     if (hasDynamicSlots())
         sizes->slots = mallocSizeOf(slots);
 
-    if (hasDynamicElements())
-        sizes->elements = mallocSizeOf(getElementsHeader());
+    if (hasDynamicElements()) {
+        js::ObjectElements *elements = getElementsHeader();
+#if defined (JS_CPU_X64)
+        // On x64, ArrayBufferObject::prepareForAsmJS switches the
+        // ArrayBufferObject to use mmap'd storage. This is not included in the
+        // total 'explicit' figure and thus we must not include it here.
+        // TODO: include it somewhere else.
+        if (JS_LIKELY(!elements->isAsmJSArrayBuffer()))
+            sizes->elements = mallocSizeOf(elements);
+#else
+        sizes->elements = mallocSizeOf(elements);
+#endif
+    }
 
     // Other things may be measured in the future if DMD indicates it is worthwhile.
     // Note that sizes->private_ is measured elsewhere.
